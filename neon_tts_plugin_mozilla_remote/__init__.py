c509428dae50cc3401ca286b3095fd96214c0c3c
Daniel McKnight <daniel@neon.ai>
2022-06-13 16:11:34 -0700

    Update to use Mycroft-compat config and OPM base class

--- neon_tts_plugin_mozilla_remote/__init__.py
+++ neon_tts_plugin_mozilla_remote/__init__.py
@@ -21,41 +21,35 @@
 
 from urllib.parse import urlencode
 from urllib.request import urlopen
-from neon_utils.configuration_utils import get_neon_tts_config
 from neon_utils.logger import LOG
 from neon_utils.parse_utils import format_speak_tags, normalize_string_to_speak
-
-try:
-    from neon_audio.tts import TTS, TTSValidator
-except ImportError:
-    from mycroft.tts import TTS, TTSValidator
-from mycroft.metrics import Stopwatch
+from ovos_plugin_manager.templates.tts import TTS, TTSValidator
+from ovos_utils.configuration import get_ovos_config
 
 
 class MozillaRemoteTTS(TTS):
-
     def __init__(self, lang="en-us", config=None):
-        config = config or get_neon_tts_config().get("mozilla", {})
-        super(MozillaRemoteTTS, self).__init__(lang, config, MozillaTTSValidator(self),
+        config = config or get_ovos_config().get("tts", {}).get("mozilla", {})
+        super(MozillaRemoteTTS, self).__init__(lang, config,
+                                               MozillaTTSValidator(self),
                                                audio_ext="wav",
                                                ssml_tags=["speak"])
         self.base_url = config.get("api_url", "http://0.0.0.0:5002/api/tts")
 
     def get_tts(self, sentence, wav_file, speaker=None):
-        stopwatch = Stopwatch()
 
         to_speak = format_speak_tags(sentence, False)
         LOG.debug(to_speak)
         if to_speak:
             url = self._build_url(normalize_string_to_speak(to_speak))
-            with stopwatch:
+            with self.stopwatch:
                 wav_data = urlopen(url).read()
-            LOG.debug(f"Request time={stopwatch.time}")
+            LOG.debug(f"Request time={self.stopwatch.time}")
 
-            with stopwatch:
+            with self.stopwatch:
                 with open(wav_file, "wb") as f:
                     f.write(wav_data)
-            LOG.debug(f"File access time={stopwatch.time}")
+            LOG.debug(f"File access time={self.stopwatch.time}")
         return wav_file, None
 
     def _build_url(self, sentence) -> str:
